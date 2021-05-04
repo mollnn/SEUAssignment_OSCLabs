e3f70834c00c7538301bbf8e82cb3b74f9e851d1
mollnn <1084918073@qq.com>
2021-05-04 14:39:44 +0800

    seush: can exec, cannot exec .sh

--- Unix_Shell/seush.c
+++ Unix_Shell/seush.c
@@ -48,6 +48,10 @@ int split(char *cmd, char **argv)
 
 int main(int argc, char *argv[])
 {
+    char pathenv[1000] = "/bin";
+    char paths[1000];
+    char path[255];
+
     if (argc > 2)
     {
         throw_error_end();
@@ -59,14 +63,16 @@ int main(int argc, char *argv[])
     }
     while (1)
     {
-
         char *av[99];
+        for (int i = 0; i < 99; i++)
+            av[i] = NULL;
         int ac;
         int pid;
         if (argc == 1)
         {
             int length;
             char input_buffer[512];
+            memset(input_buffer, 0, sizeof input_buffer);
             char prompt_string[20] = "seush>";
             write(STDOUT_FILENO, prompt_string, strlen(prompt_string));
             length = read(STDIN_FILENO, input_buffer, 512);
@@ -79,7 +85,10 @@ int main(int argc, char *argv[])
         {
             char *buf = NULL;
             size_t file_size;
+            if (feof(fp))
+                exit(0);
             getline(&buf, &file_size, fp);
+
             ac = split(buf, av);
 
             if (file_size == 0)
@@ -103,9 +112,46 @@ int main(int argc, char *argv[])
             else
                 throw_error();
         }
+        else if (ac > 0 && strcmp(av[0], "path") == 0)
+        {
+            memset(pathenv, 0, sizeof pathenv);
+            for (int i = 1; i < ac; i++)
+            {
+                strcat(pathenv, av[i]);
+                if (i + 1 < ac)
+                    strcat(pathenv, ":");
+            }
+        }
         else if ((pid = fork()) == 0)
         {
-            execv(av[0], av);
+            if (av[0][0] == '/')
+            {
+                execv(av[0], av);
+                throw_error_end();
+            }
+            else
+            {
+                strcpy(paths, pathenv);
+                char *pathitem = strtok(paths, ":");
+                int flag = 0;
+                while (pathitem)
+                {
+                    strcpy(path, pathitem);
+                    strcat(path, "/");
+                    strcat(path, av[0]);
+                    execv(path, av);
+                    pathitem = strtok(NULL, ":");
+                }
+                strcpy(pathitem, ".");
+                if (!flag)
+                {
+                    strcpy(path, pathitem);
+                    strcat(path, "/");
+                    strcat(path, av[0]);
+                    execv(path, av);
+                    pathitem = strtok(NULL, ":");
+                }
+            }
             throw_error_end();
         }
         wait(0);
