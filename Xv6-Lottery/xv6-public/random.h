6e2b744064e7952c3162a1440609f07676218c67
mollnn <1084918073@qq.com>
2021-06-15 19:15:14 +0800

    initial commit (with random)

---
+++ Xv6-Lottery/xv6-public/random.h
@@ -0,0 +1,18 @@
+#ifndef _RANDOM_H
+#define _RANDOM_H
+
+#include "types.h"
+
+static uint state[3];
+
+uint rand()
+{
+	uint x = state[0];
+	x ^= x << 13;
+	x ^= x >> 17;
+	x ^= x << 5;
+	state[0] = x;
+	return x;
+}
+
+#endif
